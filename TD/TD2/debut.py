93ddae10c401bb501bd59026bf72781c5a329962
Kélian CHAPOUTOT <kelian.chapoutot@ens.uvsq.fr>
2024-03-08 12:15:47 +0100

    TD 02

---
+++ TD/TD2/debut.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+
+CANVAS_WIDTH, CANVAS_HEIGHT = 600, 400
+
+root = tk.Tk()
+
+canvas = tk.Canvas(root, width=CANVAS_WIDTH, height=CANVAS_HEIGHT)
+
+# Début de votre code
+x0 = 100
+x1 = CANVAS_WIDTH - 100
+y = CANVAS_HEIGHT / 2
+canvas.create_line(x0, y, x1, y)
+canvas.create_oval(x0 - 50, y + 50, x0 + 50, y - 50)
+canvas.create_oval(x1 - 50, y + 50, x1 + 50, y - 50)
+canvas.create_oval((x0 + x1) / 2 - 50, y + 50, (x0 + x1) / 2 + 50, y - 50)
+
+# Fin de votre code
+
+canvas.grid(row=0, column=0)
+root.mainloop()
