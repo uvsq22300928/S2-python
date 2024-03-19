808bd2c366b06289fa8ebcec16799e5039e36c1c
uvsq22300928 <kelian.chapoutot@ens.uvsq.fr>
2024-03-19 20:07:27 +0100

    fin du code de debut.py

--- TD/TD2/debut.py
+++ TD/TD2/debut.py
@@ -1,22 +1,20 @@
-from tkinter import Tk, Canvas
-
+import tkinter as tk
 CANVAS_WIDTH, CANVAS_HEIGHT = 600, 400
 
-if __name__ == '__main__':
-    root = Tk()
+root = tk.Tk()
 
-    canvas = Canvas(root, width=CANVAS_WIDTH, height=CANVAS_HEIGHT)
+canvas = tk.Canvas(root, width=CANVAS_WIDTH, height=CANVAS_HEIGHT)
 
-    # Début de votre code
-    x0 = 100
-    x1 = CANVAS_WIDTH - 100
-    y = CANVAS_HEIGHT / 2
-    canvas.create_line(x0, y, x1, y)
-    canvas.create_oval(x0 - 50, y + 50, x0 + 50, y - 50)
-    canvas.create_oval(x1 - 50, y + 50, x1 + 50, y - 50)
-    canvas.create_oval((x0 + x1) / 2 - 50, y + 50, (x0 + x1) / 2 + 50, y - 50)
+# Début de votre code
+x0 = CANVAS_WIDTH/2
+x1 = CANVAS_WIDTH - x0
+y0 = (100*CANVAS_HEIGHT)/CANVAS_WIDTH
+y1 = CANVAS_HEIGHT - y0
+canvas.create_line(x0, y0, x1, y1)
+canvas.create_oval(x0 - 50, y0 + 50, x0 + 50, y0 - 50)
+canvas.create_oval(x1 - 50, y1 - 50, x1 + 50, y1 + 50)
+canvas.create_oval(x0 - 50, (y0 + y1) / 2 + 50, x1 + 50, (y0 + y1) / 2 - 50)
+# Fin de votre code
 
-    # Fin de votre code
-
-    canvas.pack()
-    root.mainloop()
+canvas.grid()
+root.mainloop()
