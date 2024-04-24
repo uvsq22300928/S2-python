b39a43c7a1e88ad37b2a019d6149ee6ae08c5443
uvsq22300928 <kelian.chapoutot@ens.uvsq.fr>
2024-04-24 14:43:37 +0200

    Update

---
+++ cours/06_modularité/Utils/words.py
@@ -0,0 +1,9 @@
+def compteMots(s):
+    """
+        Retourne le nombre de mots dans une phrase.
+        (un mot est une séquence non vide de caractères comprise 
+        entre deux espaces)
+    """
+    # split découpe la chaîne selon les espaces et retourne un tableau
+    # contenant les différentes parties
+    return len(s.split())
